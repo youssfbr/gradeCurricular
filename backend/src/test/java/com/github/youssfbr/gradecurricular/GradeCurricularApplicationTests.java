ae50616b4bade88e01fad0d8c6c765e819ee5936
Alisson Youssf <youssfbr@gmail.com>
2021-10-31 18:39:07 -0300

    Project created

---
+++ backend/src/test/java/com/github/youssfbr/gradecurricular/GradeCurricularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.gradecurricular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeCurricularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
