9ac698f262d8f2d1d9dd60e306fb4e6fa136e0ab
Alisson Youssf <youssfbr@gmail.com>
2021-11-02 22:08:52 -0300

    ECRUD materia

--- backend/src/main/java/com/github/youssfbr/gradecurricular/controller/MateriaController.java
+++ backend/src/main/java/com/github/youssfbr/gradecurricular/controller/MateriaController.java
@@ -1,16 +1,73 @@
 package com.github.youssfbr.gradecurricular.controller;
 
+import com.github.youssfbr.gradecurricular.entity.MateriaEntity;
+import com.github.youssfbr.gradecurricular.repository.IMateriaRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/materia")
 public class MateriaController {
 
-    @GetMapping
-    public ResponseEntity<String> get() {
-        return ResponseEntity.ok("teste");
+    private IMateriaRepository materiaRepository;
+
+    public MateriaController(IMateriaRepository materiaRepository) {
+        this.materiaRepository = materiaRepository;
     }
+
+    @GetMapping
+    public ResponseEntity<List<MateriaEntity>> listarMaterias() {
+        return ResponseEntity.ok(materiaRepository.findAll());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<MateriaEntity> consultarMateria(@PathVariable Long id) {
+        return ResponseEntity.ok(materiaRepository.findById(id).get());
+    }
+
+    @PostMapping
+    public ResponseEntity<Boolean> cadastrarMateria(@RequestBody MateriaEntity materia) {
+        try {
+            materiaRepository.save(materia);
+            return ResponseEntity.ok(true);
+        }
+        catch (Exception ex) {
+            return ResponseEntity.ok(false);
+        }
+    }
+
+    @PutMapping()
+    public ResponseEntity<Boolean> atualizarMateria(@RequestBody MateriaEntity materia) {
+        try {
+            MateriaEntity materiaEntityAtualizada = materiaRepository.findById(materia.getId()).get();
+
+            materiaEntityAtualizada.setNome(materia.getNome());
+            materiaEntityAtualizada.setCodigo(materia.getCodigo());
+            materiaEntityAtualizada.setHoras(materia.getHoras());
+            materiaEntityAtualizada.setNome(materia.getNome());
+            materiaEntityAtualizada.setFrequencia(materia.getFrequencia());
+
+            materiaRepository.save(materiaEntityAtualizada);
+
+            return ResponseEntity.ok(true);
+        }
+        catch (Exception ex) {
+            return ResponseEntity.internalServerError().body(false);
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Boolean> deletarMateria(@PathVariable Long id) {
+        try {
+            materiaRepository.deleteById(id);
+            return ResponseEntity.ok(true);
+        }
+        catch (Exception ex) {
+            return ResponseEntity.internalServerError().body(false);
+        }
+    }
+
+
 }
--- backend/src/main/java/com/github/youssfbr/gradecurricular/repository/IMateriaRepository.java
+++ backend/src/main/java/com/github/youssfbr/gradecurricular/repository/IMateriaRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MateriaRepository extends JpaRepository<MateriaEntity, Long> {
+public interface IMateriaRepository extends JpaRepository<MateriaEntity, Long> {
 }
